db236f282dbc171f86490987585c8ae69e2775d7
Anthonius Munthi <me@itstoni.com>
2018-04-08 16:29:01 +0800

    test commit hooks

--- src/Plugins/PHPBrew/Configuration.php
+++ src/Plugins/PHPBrew/Configuration.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the dotfiles project.
+ *
+ *     (c) Anthonius Munthi <me@itstoni.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Dotfiles\Plugins\PHPBrew;
 
 use Dotfiles\Core\Config\DefinitionInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-
 
 class Configuration implements DefinitionInterface
 {
